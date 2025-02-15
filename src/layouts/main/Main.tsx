55ce991338cea3a8e5ae1d8d3b8e90f40af1cd28
Lily <liliazatyan2000@gmail.com>
2025-02-15 16:31:03 +0400

    added animation

--- src/layouts/main/Main.tsx
+++ src/layouts/main/Main.tsx
@@ -1,27 +1,11 @@
 import React, { useState } from "react";
 import "./main.css";
 import "../../App.css";
-import "./landing1.png";
-import "./landing2.png";
 import ScrollToNextSection from "./ScrollToNextSection";
 
-const images = [
-  "./landing1.png",
-  "./landing2.png",
-];
+
 
 const Main: React.FC = () => {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-  const handleNextImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-  };
-
-  const handlePrevImage = () => {
-    setCurrentImageIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1
-    );
-  };
 
   return (
     <div className="landing-container">
@@ -34,14 +18,8 @@ const Main: React.FC = () => {
         </p>
       </div>
       <div className="landing-image-container">
-        <img
-          src={images[currentImageIndex]}
-          alt="Landing"
-          className={`landing-image`}
-        />
+       
         <div className="image-controls">
-          <button onClick={handlePrevImage}>Նախորդ</button>
-          <button onClick={handleNextImage}>Հաջորդ</button>
         </div>
       </div>
       <ScrollToNextSection/>
